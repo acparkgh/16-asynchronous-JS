1e0ecb630bb86142777fd849f83734109d59a1a8
acparkgh <acpark123@gmail.com>
2021-06-13 02:43:40 -0400

    code challenge 3

--- starter/script.js
+++ starter/script.js
@@ -237,4 +237,61 @@ btn.addEventListener("click", function() { whereAmI() })
 // whereAmI(19.037, 72.873)
 // whereAmI(-33.933, 18.474)
 // whereAmI(39.0395328,-76.6759214)
+
+const wait = function (second) {
+  return (
+    new Promise(function (resolve) {
+      setTimeout(resolve, second * 1000);
+    })
+  );
+};
   
+const imgContainer = document.querySelector("div.images");
+
+const createImage = function (imgPath) {
+  return (
+    new Promise(function (resolve, reject) {
+    
+      const img = document.createElement("img");
+      img.src = imgPath;
+      img.addEventListener("load", function () {
+        imgContainer.append(img);
+        resolve(img);
+      });
+      img.addEventListener("error", function () {
+        reject(new Error("Image not found"));
+      });
+    })
+  );
+};
+
+let currentImg;
+
+createImage("img/img-1.jpg")
+  .then(image => {
+    currentImg = image;
+    console.log("image 1 loaded");
+    return wait(3);
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+    return createImage("img/img-2.jpg");
+  })
+  .then(image => {
+    currentImg = image;
+    console.log("image 2 loaded");
+    return wait(3);
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+    return createImage("img/img-3.jpg");
+  })
+  .then(image => {
+    currentImg = image;
+    console.log("image 3 loaded");
+    return wait(3);
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+  })
+  .catch(error => console.log(error))
