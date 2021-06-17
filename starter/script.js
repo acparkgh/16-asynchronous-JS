cf8c0e6dfd71feafae9e24b9b126a7a29f76ee7c
acparkgh <acpark123@gmail.com>
2021-06-17 03:36:55 -0400

    code challenge 3

--- starter/script.js
+++ starter/script.js
@@ -354,32 +354,74 @@ btn.addEventListener("click", function() { whereAmI() })
 // whereAmI(-33.933, 18.474)
 // whereAmI(39.0395328,-76.6759214)
 
-// const wait = function (second) {
-//   return (
-//     new Promise(function (resolve) {
-//       setTimeout(resolve, second * 1000);
-//     })
-//   );
-// };
+const wait = function (second) {
+  return (
+    new Promise(function (resolve) {
+      setTimeout(resolve, second * 1000);
+    })
+  );
+};
   
-// const imgContainer = document.querySelector("div.images");
+const imgContainer = document.querySelector("div.images");
 
-// const createImage = function (imgPath) {
-//   return (
-//     new Promise(function (resolve, reject) {
+const createImage = function (imgPath) {
+  return (
+    new Promise(function (resolve, reject) {
     
-//       const img = document.createElement("img");
-//       img.src = imgPath;
-//       img.addEventListener("load", function () {
-//         imgContainer.append(img);
-//         resolve(img);
-//       });
-//       img.addEventListener("error", function () {
-//         reject(new Error("Image not found"));
-//       });
-//     })
-//   );
-// };
+      const img = document.createElement("img");
+      img.src = imgPath;
+      img.addEventListener("load", function () {
+        imgContainer.append(img);
+        resolve(img);
+      });
+      img.addEventListener("error", function () {
+        reject(new Error("Image not found"));
+      });
+    })
+  );
+};
+
+const loadNPause = async function () {
+  try {
+    let img = await createImage("img/img-1.jpg");
+    console.log("Image 1 loaded");
+    await wait(3);
+    img.style.display = "none";
+
+    img = await createImage("img/img-2.jpg");
+    console.log("Image 2 loaded");
+    await wait(3);
+    img.style.display = "none";
+
+    img = await createImage("img/img-3.jpg");
+    console.log("Image 3 loaded");
+    await wait(3);
+    img.style.display = "none";
+  }
+  catch (error) {
+    console.log(error);
+  };
+};
+
+// loadNPause()
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    console.log(imgs);
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+    imgsEl.forEach(img => img.classList.add("parallel"));
+  }
+  catch (error) {
+    console.error(error);
+  };
+  
+};
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
+
+
 
 // let currentImg;
 
@@ -411,3 +453,4 @@ btn.addEventListener("click", function() { whereAmI() })
 //     currentImg.style.display = "none";
 //   })
 //   .catch(error => console.log(error))
+
